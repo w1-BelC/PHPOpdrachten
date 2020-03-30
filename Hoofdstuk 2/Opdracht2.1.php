cc5ba5fbf2105307e802f258634964df02cf9550
Corné Bel <corne.bel@edu-kw1c.nl>
2020-03-30 14:16:17 +0200

    Template

---
+++ Hoofdstuk 2/Opdracht2.1.php
@@ -0,0 +1,79 @@
+<?php
+    /**
+     * User:    Corné Bel
+     * Date:    12-2-2020
+     * Time:    09:15 AM
+     * File:    Opdracht2.1.php
+     */
+    ?>
+
+<header>
+    <link href="../CSS/StyleSheet.css" type="text/css" rel="stylesheet">
+    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+</header>
+<aside>
+    <h2>Menu</h2>
+    <ul>
+        <li>Hoofdstuk 2
+            <ul>
+                <li>
+                    <a href="../Hoofdstuk%202/Opdracht2.1.php">Opdracht 2.1</a>
+                </li>
+                <li>
+                    <a href="../Hoofdstuk%202/Opdracht2.2.php">Opdracht 2.2</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+    <ul>
+        <li>Hoofdstuk 3
+            <ul>
+                <li>
+                    <a href="../Hoofdstuk%203/Opdracht3.1.php">Opdracht 3.1</a>
+                </li>
+                <li>
+                    <a href="../Hoofdstuk%203/Opdracht3.2.php">Opdracht 3.2</a>
+                </li>
+                <li>
+                    <a href="../Hoofdstuk%203/Opdracht3.3.php">Opdracht 3.3</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+    <ul>
+        <li>Hoofdstuk 4
+            <ul>
+                <li>
+                    <a href="../Hoofdstuk%204/Opdracht4.1.php">Opdracht 4.1</a>
+                </li>
+                <li>
+                    <a href="../Hoofdstuk%204/Opdracht4.2.php">Opdracht 4.2</a>
+                </li>
+                <li>
+                    <a href="../Hoofdstuk%204/Opdracht4.3.php">Opdracht 4.3</a>
+                </li>
+                <li>
+                    <a href="../Hoofdstuk%204/Opdracht4.4.php">Opdracht 4.4</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+    <ul>
+        <li>Hoofdstuk 2
+            <ul>
+                <li>
+                    <a href="../Hoofdstuk%205/Opdracht5.1.php">Opdracht 5.1</a>
+                </li>
+                <li>
+                    <a href="../Hoofdstuk%205/Opdracht5.2.php">Opdracht 5.2</a>
+                </li>
+                <li>
+                    <a href="../Hoofdstuk%205/Opdracht5.3.php">Opdracht 5.3</a>
+                </li>
+                <li>
+                    <a href="../Hoofdstuk%205/Opdracht5.4.php">Opdracht 5.4</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</aside>
